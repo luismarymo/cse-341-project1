92752c0bf71f965b3c8867ee007e0677efb15aa8
luismarymo <luchita0420@gmail.com>
2025-03-20 12:48:04 +0100

    Added validation to email field

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { userValidationRules, validate } = require("../validation.js");
 
 const contactsController = require("../controllers/contacts");
 
 router.get("/", contactsController.getAll);
 router.get("/:id", contactsController.getSingle);
 
-router.post("/", contactsController.createContact);
+router.post("/", userValidationRules(), validate, contactsController.createContact);
 
-router.put("/:id", contactsController.updateContact);
+router.put("/:id", userValidationRules(), validate, contactsController.updateContact);
 
 router.delete("/:id", contactsController.deleteContact);
 
